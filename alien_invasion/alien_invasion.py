7393d4b62658bff6cbd2ed5a437cebc2199fcdf2
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-05-11 11:16:25 +0400

    updating screen with helper func added

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -28,13 +28,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
@@ -42,6 +36,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
