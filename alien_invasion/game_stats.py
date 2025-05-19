a05d2eb96f73395ed37b4977aa64a3285a1e8701
sa6uhi <sabuhi.nazarov10@gmail.com>
2025-05-19 18:49:31 +0400

    scoring feature adding started

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -3,9 +3,10 @@ class Gamestats:
 
     def __init__(self, ai_game):
         """Initialize statistics."""
-        self.settings = ai_game.settings
+        self.ai_settings = ai_game.settings
         self.reset_stats()
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
-        self.ships_left = self.settings.ship_limit
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
